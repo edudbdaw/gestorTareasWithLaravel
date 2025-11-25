1ed996c6a256457d45b026f9ece1622016fa45f4
EduClase <edudbdawclase@gmail.com>
2025-11-25 17:29:34 +0100

    Editar y borrar prueba

--- app/Livewire/TasksComponent.php
+++ app/Livewire/TasksComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TasksComponent extends Component
 {
     public $tasks = [];
+    public $modal = false;
+    public $title;
+    public $description;
+    public $editingTask;
+    public $id;
 
     public function mount()
     {
@@ -18,6 +24,58 @@ class TasksComponent extends Component
     {
         $user = Auth::User();
         $this->tasks = $user->tasks;
+    }
+
+    public function openCreateModal(?Task $task = null)
+    {
+        if ($task) {
+            $this->editingTask = $task;
+            $this->title = $task->title;
+            $this->description = $task->description;
+            $this->id = $task->id;
+        } else {
+            $this->clearFields();
+        }
+        $this->modal = true;
+    }
+
+    public function closeCreateOrUpdateModal()
+    {
+        $this->modal = false;
+    }
+
+
+    public function createOrUpdateTask()
+    {
+        if ($this->editingTask->id) {
+            $task = Task::find($this->editingTask->id);
+            $task->update([
+                'title' => $this->title,
+                'description' => $this->description
+            ]);
+        } else {
+            Task::create([
+                'title' => $this->title,
+                'description' => $this->description,
+                'user_id' => Auth::user()->id
+            ]);
+        }
+
+        $this->closeCreateOrUpdateModal();
+        $this->clearFields();
+        $this->getTasks();
+    }
+
+    public function deleteTask(Task $task)
+    {
+        $task->delete();
+        $this->getTasks();
+    }
+
+    private function clearFields()
+    {
+        $this->title = '';
+        $this->description = '';
     }
 
     public function render()
--- resources/views/livewire/tasks-component.blade.php
+++ resources/views/livewire/tasks-component.blade.php
@@ -1,16 +1,20 @@
-<div>
-    <section>
-        <div class="container">
-            <button class="bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-700 mt-6"
-                wire:click="openCreateModal">
-                Nueva tarea
-            </button>
+<section>
+    <div class="container">
+        <!-- This is an example component -->
 
-            <div class='flex min-h-screen items-center justify-center from-white-200 via-white-300 to-white-500 bg-gradient-to-br'>
-                <div class="flex items-center justify-center min-h-[450px]">
+        <button class="bg-purple-800 text-white px-4 py-2 rounded-md hover:bg-purple-700 mt-6"
+            wire:click="openCreateModal">
+            Nueva tarea
+        </button>
+
+        <div
+            class='flex min-h-screen items-center justify-center from-white-200 via-white-300 to-white-500 bg-gradient-to-br'>
+            <div class="flex items-center justify-center min-h-[450px]">
+                <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
                     <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="py-3 px-6">Título</th>
                                     <th scope="col" class="py-3 px-6">Descripción</th>
@@ -25,6 +29,7 @@
                                         <td class="py-4 px-6"></td>
                                     </tr>
                                 @endforeach
+
                             </tbody>
                         </table>
                     </div>
@@ -32,6 +37,10 @@
             </div>
         </div>
 
+
+    </div>
+    <!-- MODAL CODE START -->
+    @if ($modal)
         <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10">
             <div class="max-h-full w-full max-w-xl overflow-y-auto sm:rounded-2xl bg-white">
                 <div class="w-full">
@@ -42,22 +51,32 @@
                                 <div class="space-y-4">
                                     <div>
                                         <label for="title" class="block text-sm font-medium text-gray-700">Título</label>
-                                        <input type="text" name="title" id="title" autocomplete="title" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                        <input type="text" wire:model="title" name="title" id="title" autocomplete="title"
+                                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                                     </div>
                                     <div>
-                                        <label for="description" class="block text-sm font-medium text-gray-700">Descripción</label>
-                                        <textarea type="text" name="description" id="description" rows="3" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
+                                        <label for="description"
+                                            class="block text-sm font-medium text-gray-700">Descripción</label>
+                                        <textarea type="text" wire:model="description" name="description" id="description"
+                                            rows="3"
+                                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
                                     </div>
-                                </div>
                             </form>
                         </div>
                         <div class="space-y-4">
-                            <button class="p-3 bg-black rounded-full text-white w-full font-semibold">Crear tarea</button>
-                            <button class="p-3 bg-white border rounded-full w-full font-semibold">Cancelar</button>
+                            <button class="p-3 bg-black rounded-full text-white w-full font-semibold"
+                                wire:click="createTask">Crear tarea
+                            </button>
+                            <button class="p-3 bg-white border rounded-full w-full font-semibold"
+                                wire:click="closeCreateModal">Cancelar</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        </section>
-</div>
+    @endif
+
+    <!-- MODAL CODE END -->
+
+
+</section>
